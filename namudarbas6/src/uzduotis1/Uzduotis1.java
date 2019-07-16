67946c57d3309216964e6c52f3bb033db90e6e79
Tomas <tcybas@gmail.com>
2019-07-16 08:32:15 +0300

    namu darbas

---
+++ namudarbas6/src/uzduotis1/Uzduotis1.java
@@ -0,0 +1,31 @@
+package uzduotis1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Uzduotis1 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner sc = new Scanner(new File("namudarbas6\\data\\uzduotis1.txt"));
+        int weeks = sc.nextInt();
+        int[][] temperatures = new int[weeks][7];
+        for (int i = 0; i < weeks; i++) {
+            for (int j = 0; j < 7; j++) {
+                temperatures[i][j] = sc.nextInt();
+            }
+        }
+
+        int [] averages = new int[weeks];
+        for (int i = 0; i < weeks; i++) {
+            for (int j = 0; j < 7; j++) {
+                averages [i] += temperatures[i][j];
+            }
+            averages [i] = averages[i]/7;
+        }
+        for (int i = 0; i < averages.length; i++){
+            System.out.println(i+1 + " savaitės temperatūra: " + averages[i]);
+        }
+
+    }
+}
---
+++ namudarbas6/src/uzduotis2/uzduotis2.java
@@ -0,0 +1,7 @@
+package uzduotis2;
+
+public class uzduotis2 {
+    public static void main(String[] args) {
+
+    }
+}
